0264ad7f7a4c9dd6735dc2a5aefa6a54a63439db
celina520524 <celina520524@163.com>
2021-07-21 09:58:52 +0800

    update

--- api/proxy.js
+++ api/proxy.js
@@ -2,21 +2,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
-  let target = ''
-  // 代理目标地址
-  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-  if (req.url.startsWith('/api')) {
-    target = process.env.GRIDSOME_API_URL
-  }
-
   // 创建代理对象并转发请求
   createProxyMiddleware({
-    target,
+    target: process.env.GRIDSOME_API_URL,
     changeOrigin: true,
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 /api
       //   例如 /api/posts 将被转发到 http://106.75.10.28:1337/posts
-      '^/api': ''
+      '^/api': '/'
     }
   })(req, res)
 }
